d03ed20e1b19f15d55fb2711b848f904a564bbb4
Stefan Georgescu <stefan.georgescu.970@gmail.com>
2019-02-04 13:55:21 +0200

    init2

---
+++ src/main/java/com/easyhelp/application/service/DoctorServiceInterface.java
@@ -0,0 +1,10 @@
+package com.easyhelp.application.service;
+
+import com.easyhelp.application.model.users.Doctor;
+
+import java.util.List;
+
+public interface DoctorServiceInterface {
+
+    List<Doctor> getAllDoctors();
+}
