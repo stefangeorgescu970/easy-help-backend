20ba78a3f6d3fb79df9d865a9645276f03360ff0
Stefan Georgescu <stefan.georgescu.970@gmail.com>
2019-03-20 18:55:43 +0200

    append commit

--- src/main/java/com/easyhelp/application/controller/DoctorController.java
+++ src/main/java/com/easyhelp/application/controller/DoctorController.java
@@ -1,20 +1,9 @@
 package com.easyhelp.application.controller;
 
-import com.easyhelp.application.model.users.Doctor;
-import com.easyhelp.application.model.users.Donor;
-import com.easyhelp.application.server.ServerResponse;
-import com.easyhelp.application.service.DoctorServiceInterface;
-import com.easyhelp.application.utils.response.Response;
-import com.easyhelp.application.utils.response.ResponseBuilder;
+import com.easyhelp.application.service.doctor.DoctorServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import springfox.documentation.annotations.ApiIgnore;
-
-import java.security.Principal;
 
 @RestController
 @RequestMapping("/doctor")
@@ -22,15 +11,6 @@ public class DoctorController {
 
     @Autowired
     private DoctorServiceInterface doctorServiceInterface;
-
-    @RequestMapping("/get")
-    private ServerResponse<Doctor> getAllDoctors() {
-        ServerResponse<Doctor> serverResponse = new ServerResponse<>();
-        serverResponse.setId("Doctors");
-        serverResponse.setAct("getAll");
-        serverResponse.setModel(doctorServiceInterface.getAllDoctors());
-        return serverResponse;
-    }
 
 }
 
