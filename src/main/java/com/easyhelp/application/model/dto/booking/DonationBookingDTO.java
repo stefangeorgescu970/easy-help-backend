74e4447b8ee56cc68e1dbd12f2c2af50bd1d8abd
Stefan Georgescu <stefan.georgescu.970@gmail.com>
2019-05-02 09:23:06 +0300

    add donation center to booking dto

--- src/main/java/com/easyhelp/application/model/dto/booking/DonationBookingDTO.java
+++ src/main/java/com/easyhelp/application/model/dto/booking/DonationBookingDTO.java
@@ -3,6 +3,7 @@ package com.easyhelp.application.model.dto.booking;
 import com.easyhelp.application.model.donations.DonationBooking;
 import com.easyhelp.application.model.dto.BaseDTO;
 import com.easyhelp.application.model.dto.account.AccountDTO;
+import com.easyhelp.application.model.dto.location.LocationDTO;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
@@ -13,6 +14,7 @@ public class DonationBookingDTO extends BaseDTO {
 
     private Long id;
     private AccountDTO donor;
+    private LocationDTO donationCenter;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
     private Date bookingDate;
@@ -21,6 +23,7 @@ public class DonationBookingDTO extends BaseDTO {
         this.id = booking.getId();
         this.donor = new AccountDTO(booking.getDonor());
         this.bookingDate = booking.getDateAndTime();
+        this.donationCenter = new LocationDTO(booking.getDonationCenter());
     }
 
 }
