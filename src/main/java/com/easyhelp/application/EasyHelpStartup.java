e1fd1f14180e5e08649772e17de24de96ae27db5
Stefan Georgescu <stefan.georgescu.970@gmail.com>
2019-05-25 11:53:43 +0300

    add new startup class

---
+++ src/main/java/com/easyhelp/application/EasyHelpStartup.java
@@ -0,0 +1,58 @@
+package com.easyhelp.application;
+
+import com.easyhelp.application.model.dto.account.RegisterDTO;
+import com.easyhelp.application.model.locations.County;
+import com.easyhelp.application.model.users.UserType;
+import com.easyhelp.application.service.RegisterService;
+import com.easyhelp.application.utils.MiscUtils;
+import com.easyhelp.application.utils.exceptions.EntityNotFoundException;
+import com.easyhelp.application.utils.exceptions.SsnInvalidException;
+import com.easyhelp.application.utils.exceptions.UserAlreadyRegisteredException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EasyHelpStartup implements ApplicationListener<ApplicationReadyEvent> {
+
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+    @Autowired
+    private RegisterService registerService;
+
+
+    public EasyHelpStartup(BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        try {
+            addSysAdmin();
+        } catch (UserAlreadyRegisteredException | SsnInvalidException | EntityNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void addSysAdmin() throws EntityNotFoundException, UserAlreadyRegisteredException, SsnInvalidException {
+        registerService.registerUser(createSysAdmin("Admin", "Adminovici", County.CLUJ, "admin", "1940205248591"));
+    }
+
+    private RegisterDTO createSysAdmin(String fn, String ln, County county, String email, String ssn) {
+        RegisterDTO sysAdmin = new RegisterDTO();
+        sysAdmin.setSkipSsnValidation(true);
+        sysAdmin.setFirstName(fn);
+        sysAdmin.setLastName(ln);
+        sysAdmin.setCounty(county);
+        sysAdmin.setEmail(email);
+        sysAdmin.setPassword(bCryptPasswordEncoder.encode("admin"));
+        sysAdmin.setSsn(ssn);
+        sysAdmin.setDateOfBirth(MiscUtils.getDataFromSsn(ssn).getDateOfBirth());
+        sysAdmin.setUserType(UserType.SYSADMIN);
+
+        return sysAdmin;
+    }
+}
